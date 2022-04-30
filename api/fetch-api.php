6f7c1a10f51fbbbb603da053b372f78c1bd81e70
Cláudio Duarte <hello@claudioduarte.pt>
2022-04-30 21:43:17 +0100

    disabled mock data

--- api/fetch-api.php
+++ api/fetch-api.php
@@ -1,8 +1,8 @@
 <?php
 
-//$json = file_get_contents("http://services.web.ua.pt/sac/senhas?format=JSON");
+$json = file_get_contents("http://services.web.ua.pt/sac/senhas?format=JSON");
 
 // ALTERNATIVE: USE MOCK DATA IF API NOT RETURNING RESULTS
-$json = file_get_contents("mockdata.json");
+//$json = file_get_contents("mockdata.json");
 
 $data = json_decode($json, true);
