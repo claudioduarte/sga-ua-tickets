4b03d11d6ed82a08a46b5d1cd772dbcdb85f0f6a
Cláudio Duarte <hello@claudioduarte.pt>
2023-05-27 14:44:28 +0100

    covid set to true

--- api/api-call.php
+++ api/api-call.php
@@ -1,5 +1,5 @@
 <?php
-$covid = false;
+$covid = true;
 $json = $covid ? file_get_contents("mockdata.json") : file_get_contents("http://services.web.ua.pt/sac/senhas?format=JSON");
 $data = json_decode($json, true);
 ?>
