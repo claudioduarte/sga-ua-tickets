ab2d153a072dc89485b69f63eccfc5222e068c8a
Cláudio Duarte <hello@claudioduarte.pt>
2025-01-09 20:51:17 +0000

    update opening and closing times

--- api/open-closed.php
+++ api/open-closed.php
@@ -4,7 +4,7 @@ if ($dayOfWeek == 0 || $dayOfWeek == 6) {
   $open = false;
 } else {
   $currentTime = date("H:i");
-  if ($currentTime >= "13:00" && $currentTime <= "14:30") {
+  if (($currentTime >= "13:00" && $currentTime <= "14:30") || ($currentTime >= "17:00" || $currentTime < "09:30")) {
     $closed = true;
     $open = false;
   } else {
